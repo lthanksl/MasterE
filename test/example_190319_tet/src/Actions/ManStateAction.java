db67bfeb493e3826491275736ead072845e14719
lthanksl <lthanksl1@naver.com>
2019-05-20 09:45:39 +0900

    임베디드 쿼리 추가

--- test/example_190319_tet/src/Actions/ManStateAction.java
+++ test/example_190319_tet/src/Actions/ManStateAction.java
@@ -1,5 +1,6 @@
 package Actions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,27 +28,41 @@ public class ManStateAction {
 	// DB처리를 수행하는 객체(DAO)
 	@Autowired
 	private DAOManState dao;
-	
+
 	@RequestMapping("/stateListJqGrid.MASE")
 	@ResponseBody
-	public Map<String, Object> test22(VOStateMan pVo ) {
+	public Map<String, Object> test22(VOStateMan pVo) {
 		VOStateMan[] vo = dao.Select(pVo);
 		Map<String, Object> maps = new HashMap<String, Object>();
-		if(vo == null)
-		{
+		if (vo == null) {
 			vo = new VOStateMan[0];
 		}
 		maps.put("data", vo);
 		/*
 		 * maps.put("total", 3); maps.put("page", 5); maps.put("records", 100);
 		 */
-		
-		
+
 		return maps;
 
 	}
+
 	@RequestMapping("/stateList.MASE")
 	public String jqGrid() {
 		return "mngManState"; // ??/mngManState.jsp호출
 	}
+
+	@RequestMapping("/stateOneS.MASE")
+	public String stateOneS(VOStateMan pVo) {
+		try {
+			ArrayList<String> para = new ArrayList<String>();
+			para.add("1");
+			/* para.add(pVo.getS_ID()); */
+			VOStateMan[] ar = dao.SelectOne(para);
+			int dd = 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "mngManState"; // ??/mngManState.jsp호출
+	}
+
 }
--- test/example_190319_tet/src/mybatis/dao/DAOManState.java
+++ test/example_190319_tet/src/mybatis/dao/DAOManState.java
@@ -1,5 +1,11 @@
 package mybatis.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,15 +13,19 @@ import java.util.Map;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 import mybatis.vo.VOStateMan;
+import util.sqlCon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DAOManState {
 
 	@Autowired
 	private SqlSessionTemplate ss;
 
-	
 	public DAOManState() {
 		System.out.println("DAOManState생성!");
 	}
@@ -37,14 +47,14 @@ public class DAOManState {
 
 		return ar;
 	}
-	
+
 	public VOStateMan[] Select(VOStateMan pVo) {
 		VOStateMan[] ar = null;
 
 		Map<String, String> maps = new HashMap<String, String>();
 		maps.put("S_SUBJECT", pVo.getS_SUBJECT());
 		maps.put("S_CATEGORY", pVo.getS_CATEGORY());
-		List<VOStateMan> list = ss.selectList("ManState.Search1",maps);
+		List<VOStateMan> list = ss.selectList("ManState.Search1", maps);
 
 		if (list != null && list.size() > 0) {
 			ar = new VOStateMan[list.size()];
@@ -71,4 +81,61 @@ public class DAOManState {
 	 * 
 	 * return ar; }
 	 */
+
+	// 1.1멤버 변수
+	// DB 연결에 필요한 객체들
+	Connection con;
+	PreparedStatement psSearch;
+	ResultSet rs;
+
+	public VOStateMan[] SelectOne(ArrayList<String> pPara) throws Exception {
+
+		ArrayList<String> columns = new ArrayList<String>();
+		StringBuilder sql = new StringBuilder(); 
+
+		columns.add("S_ID");
+		columns.add("S_SUBJECT");
+		columns.add("S_CATEGORY");
+		
+		sql.append(" SELECT * FROM STATEMAN_T  ");
+		sql.append(" 		WHERE S_ID  = ? ");
+		ArrayList<ArrayList<String>> ListAr = GetData(sql.toString(), pPara, columns);
+		VOStateMan[] ar;
+		ar = new VOStateMan[ListAr.size()];
+		for (int i = 0; i < ListAr.size(); i++) {
+			ar[i].setS_ID(ListAr.get(i).get(0));
+			ar[i].setS_SUBJECT(ListAr.get(i).get(1));
+			ar[i].setS_CATEGORY(ListAr.get(i).get(2));
+		}
+
+		return ar;
+
+	}
+
+	private ArrayList<ArrayList<String>> GetData(String sql, ArrayList<String> pPara, ArrayList<String> pColumns)
+			throws Exception, SQLException {
+		con = sqlCon.con();
+		psSearch = con.prepareStatement(sql);
+		for (int i = 1; i <= pPara.size(); i++) {
+			psSearch.setString(i, pPara.get(i-1));
+		}
+
+		rs = psSearch.executeQuery();
+
+		ArrayList<String> Lists1 = new ArrayList<String>();
+		ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+		int count =0;
+		while (rs.next()) {
+			for (int i = 0; i < pColumns.size(); i++) {
+				Lists1.add(rs.getString(pColumns.get(i)));
+				
+			}
+			data.add(Lists1);
+			count++;
+		}
+		
+		
+		return data;
+
+	}
 }
