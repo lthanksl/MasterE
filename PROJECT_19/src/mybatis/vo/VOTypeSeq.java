4ea1f8b91c109daa27b09f6aa3598c3ed71898b5
lthanksl <lthanksl1@naver.com>
2019-05-23 18:19:04 +0900

    되는것

--- PROJECT_19/src/mybatis/vo/VOTypeSeq.java
+++ PROJECT_19/src/mybatis/vo/VOTypeSeq.java
@@ -1,30 +1,38 @@
 package mybatis.vo;
 
 public class VOTypeSeq {
-	private String S_ID,S_SUBJECT,S_CATEGORY;
+	private String TS_SEQ,CLASS_TYPE,SEQ_DATE,MAX_NUM;
 
-	public String getS_ID() {
-		return S_ID;
+	public String getTS_SEQ() {
+		return TS_SEQ;
 	}
 
-	public void setS_ID(String s_ID) {
-		S_ID = s_ID;
+	public void setTS_SEQ(String tS_SEQ) {
+		TS_SEQ = tS_SEQ;
 	}
 
-	public String getS_SUBJECT() {
-		return S_SUBJECT;
+	public String getCLASS_TYPE() {
+		return CLASS_TYPE;
 	}
 
-	public void setS_SUBJECT(String s_SUBJECT) {
-		S_SUBJECT = s_SUBJECT;
+	public void setCLASS_TYPE(String cLASS_TYPE) {
+		CLASS_TYPE = cLASS_TYPE;
 	}
 
-	public String getS_CATEGORY() {
-		return S_CATEGORY;
+	public String getSEQ_DATE() {
+		return SEQ_DATE;
 	}
 
-	public void setS_CATEGORY(String s_CATEGORY) {
-		S_CATEGORY = s_CATEGORY;
+	public void setSEQ_DATE(String sEQ_DATE) {
+		SEQ_DATE = sEQ_DATE;
 	}
-	
+
+	public String getMAX_NUM() {
+		return MAX_NUM;
+	}
+
+	public void setMAX_NUM(String mAX_NUM) {
+		MAX_NUM = mAX_NUM;
+	}
+
 }
